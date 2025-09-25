5eae30dd8a091c196d946d93cc35983bbe17f88f
Kuldeep Singh <kuldeepsingh21070@gmail.com>
2025-09-25 13:09:10 +0530

    Add NoteController with CRUD operations

---
+++ src/controllers/note.controller.ts
@@ -0,0 +1,127 @@
+import { NextFunction, Request, Response } from "express";
+import { AuthRequest } from "./auth.controller";
+import { AuthorizationError, ValidationError } from "../utils/errors";
+import { NoteService } from "../services/note.service";
+
+
+
+export class NoteController {
+    static async create(req: AuthRequest, res: Response, next: NextFunction){
+        try {
+            const { title, content} = req.body
+            if(!title || !content) {
+                throw new ValidationError("All the fields are required")
+            }
+
+            const note = await NoteService.create({
+                title,
+                content,
+                userId: req.user!._id.toString()
+            })
+
+            res.status(201).json({
+                success: true,
+                data: { note},
+                message: 'Note created successfully'
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    static async getAll(req: AuthRequest, res: Response, next: NextFunction){
+        try {
+            const page = parseInt(req.query.page as string) || 1
+            const limit = parseInt(req.query.limit as string) || 10
+
+            const result = await NoteService.getAll(
+                req.user!._id.toString(),
+                page,
+                limit
+            )
+            res.json({
+                success: true,
+                data: result,
+                message: 'Notes retrieved successfully'
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    static async getById(req: AuthRequest, res: Response, next:NextFunction){
+        try {
+            const { id} = req.params
+            const note = await NoteService.getById(
+                id,
+                req.user!._id.toString()
+            )
+
+            res.json({
+                success: true,
+                data: {note},
+                message: "Note retrieved successfully"
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    static async update(req: AuthRequest, res: Response, next: NextFunction){
+        try {
+            const { id }= req.params
+            const { title, content} = req.body
+
+            const note = await NoteService.update(
+                id,
+                req.user!._id.toString(),
+                { title, content}
+            )
+
+            res.json({
+                success: true,
+                data: { note},
+                message: 'note updated successfully'
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    static async delete(req: AuthRequest, res: Response, next: NextFunction){
+        try {
+            const { id } = req.params
+            await NoteService.delete(
+                id,
+                req.user!._id.toString()
+            )
+
+            res.json({
+                success: true,
+                message: 'Note deleted successfully'
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+    static async getAllAdmin(req: AuthRequest, res: Response, next: NextFunction){
+        try {
+            if(req.user!.role !== 'admin') {
+                throw new AuthorizationError('admin access required')
+            }
+    
+            const page = parseInt(req.query.page as string) || 1
+            const limit = parseInt(req.query.limit as string) || 20
+    
+            const result = await NoteService.getAllAdmin(page,limit)
+    
+            res.json({
+                success: true,
+                data: result,
+                message: 'All notes retrieved successfullly'
+            })
+        } catch (error) {
+            next(error)
+        }
+    } 
+}
